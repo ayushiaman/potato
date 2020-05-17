01827c1d859fc41cc0129b37b269087e1c335aff
ayushiaman <47924709+ayushiaman@users.noreply.github.com>
2020-05-17 13:00:12 -0400

    Create creds.js

---
+++ creds.js
@@ -0,0 +1,68 @@
+#teamleader members credentials for team intuit
+
+const user6 = {
+  firstName: 'Sonny',
+  lastName: 'Banshee',
+  email: 'infoneil4@gmail.com',
+  password: 'Roorkee@123',
+  dob: '2001-04-30',
+  gender: 'female',
+ 
+};
+
+const user1 = {
+  firstName: 'test',
+  lastName: 'Tester',
+  email: 'mrtest@gmail.com',
+  password: 'testing1',
+  gender: 'male',
+  dob: '2001-04-30',  
+};
+
+const user2 = {
+  firstName: 'Mrstest',
+  lastName: 'tester',
+  email: 'test@test.com',
+  password: 'testing2',
+  gender: 'female',
+  dob: '2001-04-30',  
+};
+
+const user3 = {
+  first: 'olademi',
+  lastName: 'tester',
+  email: 'tj@gmail.com',
+  password: 'testing222',
+  gender: 'female',
+  dob: '2001-04-30',
+};
+
+const user4 = {
+  firstName: 'olademi',
+  lastName: 'tester',
+  email: 'olatj@gmail.com',
+  password: 'testing222',
+  gender: 'female',
+  dob: '2001-04-30',
+};
+
+const user5 = {
+  firstName: 'olademi',
+  lastName: 'tester',
+  email: 'olatj@gmail.com',
+  password: 'testing222',
+  gender: 'female',
+  dob: '2001-04-30',
+
+  companyName: 'PWC',
+  address: '4th, saint john str',
+};
+
+const user7 = {
+  firstName: 'ola1',
+  lastName: 'tester',
+  email: 'max@gmail.com',
+  password: 'testing222',
+  gender: 'female',
+  dob: '2016-01-01T00:00:00-06:00',
+};
